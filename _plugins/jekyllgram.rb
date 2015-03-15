83723cdd81dee2aee962335c54751cfdeb6620cc
Ben Barber <contact@benbarber.co.uk>
2015-03-15 14:10:25 +0000

    Initial commit

---
+++ _plugins/jekyllgram.rb
@@ -0,0 +1,83 @@
+#
+# JekyllGram
+#
+# A Jekyll plugin to pull in your latest Instagram photos
+# v0.01
+# http://benbarber.uk
+# Copyright 2015 Ben Barber
+# MIT License
+#
+# Setup:
+#
+# To use this plugin you will need to make your Instagram API credentials
+# available as environment variables below:
+#
+# ENV['JEKYLLGRAM_USER'] = {{ INSTAGRAM_USER_ID }}
+# ENV['JEKYLLGRAM_KEY'] = {{ INSTAGRAM_CLIENT_ID }}
+#
+# Usage in your templates:
+#
+# {% jekyllgram 6 %}
+#    <a href="{{ photo.link }}" title="{{ photo.caption.text }}">
+#      <img src="{{ photo.images.thumbnail.url }}"
+#           title="{{ photo.caption.text }}" />
+#    </a>
+# {% endjekyllgram %}
+#
+
+require 'jekyll'
+require 'net/http'
+require 'json'
+
+module Jekyll
+  # _plugins/jekyllgram.rb
+  class JekyllGram < Liquid::Block
+
+    include Liquid::StandardFilters
+
+    def initialize(tag, params, token)
+      @limit = params.to_i | 6
+      @user_id = ENV['JEKYLLGRAM_USER']
+      @client_id = ENV['JEKYLLGRAM_KEY']
+      @api_url = 'https://api.instagram.com/v1'
+
+      super
+    end
+
+    def render(context)
+      context.registers[:jekyllgram] ||= Hash.new(0)
+      content = generate(context, recent_photos)
+
+      content
+    end
+
+    private
+
+    def generate(context, photos)
+      result = []
+
+      context.stack do
+        photos.each_with_index do |photo, index|
+          context['photo'] = photo
+          result << render_all(@nodelist, context)
+
+          break if index + 1 == @limit
+        end
+      end
+
+      result
+    end
+
+    def recent_photos
+      method = "/users/#{@user_id}/media/recent"
+      keys = "/?client_id=#{@client_id}"
+
+      resp = Net::HTTP.get_response(URI.parse(@api_url + method + keys))
+      resp = JSON.parse(resp.body)
+
+      resp['data']
+    end
+  end
+end
+
+Liquid::Template.register_tag('jekyllgram', Jekyll::JekyllGram)
